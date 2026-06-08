386f1d15c505f5672c08a88d271eb734f0002fce
Aengus MacKenzie <aengus.2000@gmail.com>
2026-06-08 13:43:43 -0400

    Add medication schema

--- server/app/schemas/__init__.py
+++ server/app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from app.schemas.user_schema import UserSchema
 from app.schemas.pet_schema import PetSchema
 from app.schemas.care_routine_schema import CareRoutineSchema
+from app.schemas.medication_schema import MedicationSchema
