33668fc67cc4179aedc4c824f1af345724d9d1a4
Aengus MacKenzie <aengus.2000@gmail.com>
2026-06-08 14:26:23 -0400

    Add password hashing helpers

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,4 +1,4 @@
-from app.extensions import db
+from app.extensions import db, bcrypt
 
 
 class User(db.Model):
@@ -14,6 +14,17 @@ class User(db.Model):
         back_populates="user",
         cascade="all, delete-orphan"
     )
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not readable.")
+    
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password.encode("utf-8")).decode("utf-8")
+
+    def authenticate(self, plain_text_password):
+        return bcrypt.check_password_hash(self.password_hash, plain_text_password.encode("utf-8"))
 
     def __repr__(self):
         return f"<User {self.id}: {self.username}>"
